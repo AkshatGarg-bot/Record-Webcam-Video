bb4c6197a5e55543761b7ad1fd46de60424b2309
Akshat <akshat.garg@iiitb.ac.in>
2021-07-31 00:57:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+import {useState}  from 'react';
+import {useEffect} from 'react';
+import { useRecordWebcam } from 'react-record-webcam';
 
-function App() {
+
+function App(props) {
+  const recordWebcam = useRecordWebcam();
+
+  // const saveFile = async () => {
+  //   const blob = await recordWebcam.getRecording();}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>afsdsf {socketid}
+        
+      </h1> */}
+      {/* <video autoPlay ></video> */}
+      <p>Camera status: {recordWebcam.status}</p>
+      <button onClick={recordWebcam.open}>Open camera</button>
+      <button onClick={recordWebcam.start}>Start recording</button>
+      <button onClick={recordWebcam.stop}>Stop recording</button>
+      <button onClick={recordWebcam.retake}>Retake recording</button>
+      <button onClick={recordWebcam.download}>Download recording</button>
+      {/* <button onClick={saveFile}>Save file to server</button> */}
+      <video ref={recordWebcam.webcamRef} autoPlay muted />
+      <video ref={recordWebcam.previewRef} autoPlay muted loop />
     </div>
+    
   );
 }
 
